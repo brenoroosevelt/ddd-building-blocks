40dadf50bce756d64fbbfce9a40414f5bef00ff7
Breno Roosevelt <breno.jesus@ufms.br>
2021-06-25 12:27:34 -0400

    initial commit

--- src/Domain/Specification/Spec.php
+++ src/Domain/Specification/Spec.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\DDD\BuildingBlocks\Domain\Specification;
 
-class Spec
+final class Spec
 {
     public static function and(Specification $specification, Specification ...$specifications): Specification
     {
