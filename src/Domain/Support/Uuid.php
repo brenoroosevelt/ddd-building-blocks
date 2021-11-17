043f1a1b2aca86f7d1c3765eb4ea0d2aa923a4ac
Breno Roosevelt <breno.jesus@ufms.br>
2021-11-17 14:18:52 -0400

    initial commit

--- src/Domain/Support/Uuid.php
+++ src/Domain/Support/Uuid.php
@@ -3,10 +3,11 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\DDD\BuildingBlocks\Domain\Support;
 
+use BrenoRoosevelt\DDD\BuildingBlocks\Domain\Identity;
 use BrenoRoosevelt\DDD\BuildingBlocks\Domain\Validation\Constraint;
 use BrenoRoosevelt\DDD\BuildingBlocks\Domain\Validation\Constraints\UuidValidator as UuidValidation;
 
-class Uuid extends StringLiteral
+class Uuid extends StringLiteral implements Identity
 {
     protected string $id;
 
