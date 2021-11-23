4d521982be1274ca65e517277bf506de062fc021
Breno Roosevelt <breno.jesus@ufms.br>
2021-11-23 14:45:49 -0400

    initial commit

--- src/Infrastructure/Doctrine/Sorting.php
+++ src/Infrastructure/Doctrine/Sorting.php
@@ -5,27 +5,28 @@ namespace BrenoRoosevelt\DDD\BuildingBlocks\Infrastructure\Doctrine;
 
 use Doctrine\ORM\QueryBuilder;
 
-class OrderBy
+class Sorting
 {
-    public function __construct(private array $allowedFields = [], private array $default = [])
+    public function __construct(private array $fields = [], private array $default = [])
     {
     }
 
-    private function setOrderBy(
+    public function setOrderBy(
         QueryBuilder|\Doctrine\DBAL\Query\QueryBuilder $queryBuilder,
         array $orderBy
     ): QueryBuilder {
         $orderBy =
-            array_filter($orderBy, fn($v, $i) => in_array($i, $this->allowedFields), ARRAY_FILTER_USE_BOTH);
+            array_filter($orderBy, fn($v, $i) => array_key_exists($i, $this->fields), ARRAY_FILTER_USE_BOTH);
 
         $orderBy = !empty($orderBy) ? $orderBy : $this->default;
-        foreach ($orderBy as $field => $direction) {
+        foreach ($orderBy as $alias => $direction) {
             $direction =
                 match (mb_strtolower((string) $direction)) {
                     'desc' => 'desc',
                     default => 'asc'
                 };
 
+            $field = $this->fields[$alias];
             $queryBuilder = $queryBuilder->orderBy($field, $direction);
         }
 
