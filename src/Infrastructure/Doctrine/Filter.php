8aff8cea942201dbf17edcc069e0db685e00c54b
Breno Roosevelt <breno.jesus@ufms.br>
2021-11-23 15:31:28 -0400

    initial commit

---
+++ src/Infrastructure/Doctrine/Filter.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\DDD\BuildingBlocks\Infrastructure\Doctrine;
+
+use Doctrine\ORM\QueryBuilder;
+
+class Filter
+{
+    private function __construct(private array $fields = [])
+    {
+    }
+
+    public static function new(array $fields = []): self
+    {
+        return new self($fields);
+    }
+
+    public function add(string $alias, callable $fn): self
+    {
+        $this->fields[$alias] = $fn;
+        return $this;
+    }
+
+    public function apply(
+        QueryBuilder|\Doctrine\DBAL\Query\QueryBuilder $queryBuilder,
+        array $filters = []
+    ): QueryBuilder {
+        foreach ($filters as $alias => $value) {
+            $filter = $this->filters[$alias] ?? fn ($q, $v, $f) => $q;
+            $queryBuilder = $filter($queryBuilder, $value, $filters);
+        }
+
+        return $queryBuilder;
+    }
+}
