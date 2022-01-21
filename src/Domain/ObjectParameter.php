6744b2be80bd269926398795764de98c64f90d55
Breno Roosevelt <breno.jesus@ufms.br>
2022-01-21 19:25:25 -0400

    initial commit

---
+++ src/Domain/ObjectParameter.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\DDD\BuildingBlocks\Domain;
+
+use DateTimeImmutable;
+
+interface ObjectParameter
+{
+    public function has(string $key): bool;
+    public function get(string $key, $default = null): mixed;
+    public function getString(string $key, ?string $default = null): ?string;
+    public function getInt(string $key, ?int $default = null): ?int;
+    public function getFloat(string $key, ?float $default = null): ?float;
+    public function getBoolean(string $key, ?bool $default = null): ?bool;
+    public function getDateTime(string $key, ?string $default = null): ?DateTimeImmutable;
+}
