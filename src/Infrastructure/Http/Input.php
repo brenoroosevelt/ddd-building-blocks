66593131bf629fcc6c4083829521cd945abedb2d
Breno Roosevelt <breno.jesus@ufms.br>
2022-01-21 15:46:20 -0400

    initial commit

--- src/Infrastructure/Http/Input.php
+++ src/Infrastructure/Http/Input.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\DDD\BuildingBlocks\Infrastructure\Http;
 
+use ArrayAccess;
+use ArrayIterator;
 use DateTimeImmutable;
+use IteratorAggregate;
 use Psr\Http\Message\ServerRequestInterface;
+use RuntimeException;
 
-class Input
+class Input implements ArrayAccess, IteratorAggregate
 {
     public array $query;
     public array $body;
@@ -22,6 +26,13 @@ class Input
     public function get(string $key, $default = null)
     {
         return $this->query[$key] ?? $this->body[$key] ?? $this->attributes[$key] ?? $default;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->query)
+            || array_key_exists($key, $this->body)
+            || array_key_exists($key, $this->attributes);
     }
 
     public function getString(string $key, ?string $default = null): ?string
@@ -53,5 +64,39 @@ class Input
         $value = $this->get($key);
         $defaultValue = $default !== null ? new DateTimeImmutable($default) : null;
         return $value !== null ? new DateTimeImmutable($value) : $defaultValue;
+    }
+
+    public function all(): array
+    {
+        return [
+            ...$this->query,
+            ...$this->body,
+            ...$this->attributes
+        ];
+    }
+
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->all());
+    }
+
+    public function offsetExists(mixed $offset)
+    {
+        return $this->has($offset);
+    }
+
+    public function offsetGet(mixed $offset)
+    {
+        return $this->get($offset);
+    }
+
+    public function offsetSet(mixed $offset, mixed $value)
+    {
+        throw new RuntimeException('Read only input: cannot set values');
+    }
+
+    public function offsetUnset(mixed $offset)
+    {
+        throw new RuntimeException('Read only input: cannot unset values');
     }
 }
