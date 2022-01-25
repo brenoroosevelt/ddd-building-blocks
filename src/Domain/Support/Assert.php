e8d456072a4b9ef6b8448691047697c3c1775edc
Breno Roosevelt <breno.jesus@ufms.br>
2022-01-25 08:30:36 -0400

    initial commit

--- src/Domain/Support/Assert.php
+++ src/Domain/Support/Assert.php
@@ -24,6 +24,26 @@ final class Assert
         }
     }
 
+    public static function equals($v1, $v2, string|Throwable $error): void
+    {
+        Assert::true($v1 == $v2, $error);
+    }
+
+    public static function notEquals($v1, $v2, string|Throwable $error): void
+    {
+        Assert::true($v1 != $v2, $error);
+    }
+
+    public static function exacty($v1, $v2, string|Throwable $error): void
+    {
+        Assert::true($v1 === $v2, $error);
+    }
+
+    public static function notExacty($v1, $v2, string|Throwable $error): void
+    {
+        Assert::true($v1 !== $v2, $error);
+    }
+
     public static function empty($value, string|Throwable $error): void
     {
         Assert::true(empty($value), $error);
@@ -67,6 +87,59 @@ final class Assert
     public static function between($i, $f, $value, string|Throwable $error): void
     {
         Assert::true($value >= $i && $value <= $f, $error);
+    }
+
+    public static function string($value, string|Throwable $error): void
+    {
+        Assert::true(is_string($value), $error);
+    }
+
+    public static function boolean($value, string|Throwable $error): void
+    {
+        Assert::true(is_bool($value), $error);
+    }
+
+    public static function int($value, string|Throwable $error): void
+    {
+        Assert::true(is_int($value), $error);
+    }
+
+    public static function float($value, string|Throwable $error): void
+    {
+        Assert::true(is_float($value), $error);
+    }
+
+    public static function numeric($value, string|Throwable $error): void
+    {
+        Assert::true(is_numeric($value), $error);
+    }
+
+    public static function instanceOf($objectOrClass, $value, string|Throwable $error): void
+    {
+        Assert::true(is_subclass_of($value, $objectOrClass, true), $error);
+    }
+
+    public static function allInstanceOf($objectOrClass, iterable $values, string|Throwable $error): void
+    {
+        foreach ($values as $value) {
+            Assert::instanceOf($objectOrClass, $value, $error);
+        }
+    }
+
+    public static function match($pattern, $value, string|Throwable $error): void
+    {
+        Assert::true(preg_match($pattern, $value) === 1, $error);
+    }
+
+    public static function inList(iterable $list, $value, string|Throwable $error): void
+    {
+        Assert::true(Arr::some($list, fn($el) => $el === $value), $error);
+    }
+
+    public static function dateTimeFormat(string $format, string $value, string|Throwable $error): void
+    {
+        $d = \DateTime::createFromFormat($format, $value);
+        Assert::true($d && $d->format($format) === $value, $error);
     }
 
     private static function throw(string|Throwable $error): void
